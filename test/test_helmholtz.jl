a0bf9d5054676f94d98b20ba86ed517384fc0ed6
Mikkel Paltorp Schmitt <mpasc@elektro.dtu.dk>
2024-02-20 07:39:28 +0100

    Update test_helmholtz.jl

--- test/test_helmholtz.jl
+++ test/test_helmholtz.jl
@@ -14,15 +14,15 @@ Random.seed!(0)
     dipstrs = rand(N) + 1im*rand(N)
     dipvecs = ones(2, N)/sqrt(2)
     # Direct computations (N^2)
-    refpot = FMM2D.h2ddir(zk,sources,sources;dipvecs=dipvecs,dipstr=dipstrs,charges=charges,pgt=3)
+    refpot = FMM2D.h2ddir(zk=zk,sources=sources,targets=sources,dipvecs=dipvecs,dipstr=dipstrs,charges=charges,pgt=3)
 
     M = 100
     targets = rand(2, M)
     # Direct computations (N^2)
-    refpottarg = FMM2D.h2ddir(zk,sources,targets;dipvecs=dipvecs,dipstr=dipstrs,charges=charges,pgt=3)
+    refpottarg = FMM2D.h2ddir(zk=zk,sources=sources,targets=targets,dipvecs=dipvecs,dipstr=dipstrs,charges=charges,pgt=3)
     for tol_exp=-14:-1
         tolerance = 0.49*10.0^tol_exp
-        U = hfmm2d(tolerance,zk, sources; charges=charges, dipstr=dipstrs, dipvecs=dipvecs,pg=3)
+        U = hfmm2d(eps=tolerance,zk=zk,sources=sources, charges=charges, dipstr=dipstrs, dipvecs=dipvecs,pg=3)
         pot_relerr  = norm(U.pot  - refpot.pottarg)  / norm(refpot.pottarg)
         grad_relerr = norm(U.grad - refpot.gradtarg) / norm(refpot.gradtarg)
         hess_relerr = norm(U.hess - refpot.hesstarg) / norm(refpot.hesstarg)
@@ -30,7 +30,7 @@ Random.seed!(0)
         @test grad_relerr < tolerance
         @test hess_relerr < tolerance
 
-        Utarg = hfmm2d(tolerance,zk, sources; charges=charges, dipstr=dipstrs, dipvecs=dipvecs,pgt=3,targets=targets)
+        Utarg = hfmm2d(eps=tolerance,zk=zk,sources=sources, charges=charges, dipstr=dipstrs, dipvecs=dipvecs,pgt=3,targets=targets)
         pot_relerrtarg  = norm(Utarg.pottarg  - refpottarg.pottarg)  / norm(refpottarg.pottarg)
         grad_relerrtarg = norm(Utarg.gradtarg - refpottarg.gradtarg) / norm(refpottarg.gradtarg)
         hess_relerrtarg = norm(Utarg.hesstarg - refpottarg.hesstarg) / norm(refpottarg.hesstarg)
